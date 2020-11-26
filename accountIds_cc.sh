44737a33baec78915c743a4e5e15ac7a168425a6
Konrad Wallace <59604517+KW-Cloud-Tech@users.noreply.github.com>
2020-11-27 09:23:52 +1100

    Add files via upload

---
+++ accountIds_cc.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# Map AWS account numbers to Conformity Account ids
+
+echo "Which region is your conformity environment hosted in?"
+read -r region
+
+echo "Enter your api key: "
+read -r apikey
+
+# select which Conformity accounts to run against using arguments
+if [ "$#" -eq  "0" ]
+then
+    printf "\nNo accountid arguments specified, generating report across all accounts loaded in conformity\n"
+    export accountid=(`curl -L -X GET \
+        "https://$region-api.cloudconformity.com/v1/accounts" \
+        -H "Content-Type: application/vnd.api+json" \
+        -H "Authorization: ApiKey $apikey" \
+        | jq -r '.data | map(.id) | join(",")'`)
+	echo "\nWill generate report for the following accounts $accountid"
+else #run against only specified accountids in argument
+    export accountid=$1
+	printf "\nGenerating cost report for account(s) $1\n"
+fi
+
+# print a list of accounts and their AWS account numbers
+TIMESTAMP=`date +%Y%m%d-%H%M%S`
+curl -L -X GET \
+        "https://$region-api.cloudconformity.com/v1/accounts" \
+        -H "Content-Type: application/vnd.api+json" \
+        -H "Authorization: ApiKey $apikey" \
+	| jq -r '.data[] | {"Conformity-ID" : .id, "AWSAccount" : .attributes | .["awsaccount-id"]} | keys_unsorted, map(.) | @csv' | awk 'NR==1 || NR%2==0'  >> AWS_CC_mapping_$TIMESTAMP.csv
+printf "\n'AWS_CC_mapping$TIMESTAMP.csv' created\n"
